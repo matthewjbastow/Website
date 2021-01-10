09919746e967a25aabaf5eedae1ed464a858933a
matthewjbastow <77070292+matthewjbastow@users.noreply.github.com>
2021-01-10 17:35:45 +0000

    Update 10/1

--- collapsibleScript.js
+++ collapsibleScript.js
@@ -4,5 +4,15 @@ for (var i = 0; i < collapsibles.length; i++) {
   collapsibles[i].onclick = function() {
     this.classList.toggle("active");
     this.nextElementSibling.classList.toggle("show");
+    hideAll(this);
   };
+}
+
+function hideAll(exceptThis) {
+	for (var i = 0; i < collapsibles.length; i++) {
+		if (collapsibles[i] !== exceptThis) {
+			collapsibles[i].classList.remove("active");
+			collapsibles[i].nextElementSibling.classList.remove("show");
+		}
+	}
 }
